cdd7199953aa070cbc590bceb8a2a55ec389b6d6
rona <row.none@gmail.com>
2023-06-26 11:38:01 +0800

    remove codes

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,24 +9,7 @@ export const metadata = {
   description: 'devfund.net - Join our exclusive community of like minded people on devfund.net',
 }
 
-export async function generateMetadata({ params, searchParams }, parent) {
-  // read route params
-  const id = params.id
-  console.log('params',params)
-  console.log('searchParams',params)
-  // fetch data
-  //const product = await fetch(`https://.../${id}`).then((res) => res.json())
- 
-  // optionally access and extend (rather than replace) parent metadata
-  //const previousImages = (await parent).openGraph?.images || []
- 
-  return {
-    title: 'hello',
-  }
-}
- 
-
-export default function RootLayout({ children, params, searchParams }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
