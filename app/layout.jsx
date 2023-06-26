13e8c3c24ff753eacfd09e61dee264208181010d
rona <row.none@gmail.com>
2023-06-26 11:48:39 +0800

    added variable

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,9 @@ import './globals.css';
 import Navigation from '../components/navigation';
 import Footer from '../components/footer';
 
-
+try{
+	console.log('NEXT_PUBLIC_VERCEL_URL',process.env.NEXT_PUBLIC_VERCEL_URL);
+}catch(e){}
 export const metadata = {
   title: 'Welcome to devfund.net',
   description: 'devfund.net - Join our exclusive community of like minded people on devfund.net',
