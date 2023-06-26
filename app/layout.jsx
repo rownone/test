00ad454395da21ec6a8c4fe97e56f55170fdc32a
rona <row.none@gmail.com>
2023-06-26 16:39:54 +0800

    fixed metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,11 +5,6 @@ const config = {
 	DOMAIN: process.env.NEXT_PUBLIC_VERCEL_URL
 };
 
-export const metadata = {
-  title: 'Welcome to Contrib.com',
-  description: 'Contrib.com - Join our exclusive community of like minded people on contrib.com'
-}
-
 export async function generateMetadata({ params, searchParams }, parent) {
 	
 	const url = `https://api1.contrib.co/v2/domains/getdomainconfig?key=5c1bde69a9e783c7edc2e603d8b25023&domain=${config.DOMAIN}`
