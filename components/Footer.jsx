f5fdb8ef0e58bbf954e3a6e9b98cd545d365385c
rona <row.none@gmail.com>
2023-06-29 21:27:34 +0800

    restore files

---
+++ components/Footer.jsx
@@ -0,0 +1,55 @@
+import Link from "next/link"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse,faBookmark,faBook,faCube } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = ({domain}) => {
+  return (
+    <footer className="tw-text-white tw-bg-black tw-py-6">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6">
+            &copy; {new Date().getFullYear()} {domain}. All Rights Reserved.
+          </div>
+          <div className="col-xl-6 text-md-end">
+            <ul className="mb-0 list-inline">
+              <li className="list-inline-item">
+                <Link href="/" className="tw-no-underline tw-capitalize tw-text-white tw-inline-flex tw-items-center hover:tw-text-white/75">
+                  <FontAwesomeIcon icon={faHouse} className="tw-w-4 tw-h-4 tw-mr-2" />
+                  <span>
+                    home
+                  </span>
+                </Link>
+              </li>
+              <li className="list-inline-item">
+                <Link href="/about" className="tw-no-underline tw-capitalize tw-text-white tw-inline-flex tw-items-center hover:tw-text-white/75">
+                  <FontAwesomeIcon icon={faBookmark} className="tw-w-4 tw-h-4 tw-mr-2" />
+                  <span>
+                    about us
+                  </span>
+                </Link>
+              </li>
+              <li className="list-inline-item">
+                <Link href="/terms" className="tw-no-underline tw-capitalize tw-text-white tw-inline-flex tw-items-center hover:tw-text-white/75">
+                  <FontAwesomeIcon icon={faBook} className="tw-w-4 tw-h-4 tw-mr-2" />
+                  <span>
+                    terms
+                  </span>
+                </Link>
+              </li>
+              <li className="list-inline-item">
+                <Link href="/privacy" className="tw-no-underline tw-capitalize tw-text-white tw-inline-flex tw-items-center hover:tw-text-white/75">
+                  <FontAwesomeIcon icon={faCube} className="tw-w-4 tw-h-4 tw-mr-2" />
+                  <span>
+                    privacy
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Logo.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function Logo({domain,logo}) {
+  return(
+    <>
+      {
+        logo ? <Image 
+              src={logo}
+              width={300}
+              height={300}
+              alt=""
+              className='d-inline-flex img-fluid mb-3'
+            /> : <h1 className="tw-text-5xl font-800">{domain}</h1>}
+      <p className="tw-text-2xl">
+        <a href="https://contrib.com/" className="tw-no-underline text-primary">
+        Proud Member of CONTRIB
+        </a>
+        {" "}|{" "}
+        <a href="https://vnoc.com/" className="tw-no-underline text-primary">
+        Powered by VNOC
+        </a>
+      </p>
+    </>
+  )
+}
